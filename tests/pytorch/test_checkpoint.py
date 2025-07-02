e5c811501b0f14752f2acc38df5a573c422a7018
Tim Moon <tmoon@nvidia.com>
2025-07-02 23:45:14 +0000

    Remove checkpoint files
    Signed-off-by: Tim Moon <tmoon@nvidia.com>

--- tests/pytorch/test_checkpoint.py
+++ tests/pytorch/test_checkpoint.py
@@ -6,17 +6,14 @@ from __future__ import annotations
 
 import argparse
 import functools
+import os
 import pathlib
-import sys
 
 import pytest
 import torch
 
 import transformer_engine.pytorch as te
 
-# Import utility functions
-_current_dir = pathlib.Path(__file__).resolve().parent
-sys.path.append(str(_current_dir))
 from utils import make_recipe
 
 # Check supported quantization schemes
@@ -52,7 +49,7 @@ class TestLoadCheckpoint:
 
     @staticmethod
     def _make_module(name: str) -> torch.nn.Module:
-        """ "Construct a module"""
+        """Construct a module"""
         if name == "linear":
             return te.Linear(1, 1)
         if name == "layernorm_linear":
@@ -82,15 +79,31 @@ class TestLoadCheckpoint:
         raise ValueError(f"Unrecognized module name ({name})")
 
     @staticmethod
-    def _checkpoint_file(name: str) -> pathlib.Path:
-        """Path to a module's checkpoint file"""
-        return _current_dir / "checkpoints" / f"{name}.pt"
+    @functools.lru_cache(maxsize=None)
+    def _checkpoint_dir() -> pathlib.Path:
+        """Path to directory with checkpoint files"""
+
+        # Check environment variable
+        path = os.getenv("TE_TEST_CHECKPOINT_ARTIFACT_PATH")
+        if path:
+            return pathlib.Path(path).resolve()
+
+        # Fallback to path in root dir
+        root_dir = pathlib.Path(__file__).resolve().parent.parent.parent
+        return root_dir / "artifacts" / "tests" / "pytorch" / "test_checkpoint"
 
     @staticmethod
-    def _save_checkpoint(name: str) -> None:
+    def _save_checkpoint(name: str, checkpoint_dir: Optional[pathlib.Path] = None) -> None:
         """Save a module's checkpoint file"""
+
+        # Path to save checkpoint
+        if checkpoint_dir is None:
+            checkpoint_dir = TestLoadCheckpoint._checkpoint_dir()
+        checkpoint_dir.mkdir(exist_ok=True)
+        checkpoint_file = checkpoint_dir / f"{name}.pt"
+
+        # Create module and save checkpoint
         module = TestLoadCheckpoint._make_module(name)
-        checkpoint_file = TestLoadCheckpoint._checkpoint_file(name)
         torch.save(module.state_dict(), checkpoint_file)
         print(f"Saved checkpoint for {name} at {checkpoint_file}")
 
@@ -111,7 +124,7 @@ class TestLoadCheckpoint:
         module = self._make_module(name)
 
         # Load checkpoint from file
-        checkpoint_file = self._checkpoint_file(name)
+        checkpoint_file = self._checkpoint_dir() / f"{name}.pt"
         if not checkpoint_file.is_file():
             raise FileNotFoundError(f"Could not find checkpoint file at {checkpoint_file}")
         state_dict = torch.load(checkpoint_file, weights_only=False)
@@ -133,17 +146,29 @@ def main() -> None:
         "--save-checkpoint",
         type=str,
         default=None,
-        help="Save a checkpoint file for a module",
+        help="Save checkpoint file for a module",
+    )
+    parser.add_argument(
+        "--checkpoint-dir",
+        type=str,
+        default=None,
+        help="Directory to save checkpoint file in",
     )
     args = parser.parse_args()
 
     # Save checkpoint files if needed
     if args.save_checkpoint is not None:
+        checkpoint_dir = args.checkpoint_dir
+        if checkpoint_dir is not None:
+            checkpoint_dir = pathlib.Path(checkpoint_dir).resolve()
         if args.save_checkpoint == "all":
             for name in _TestLoadCheckpoint_name_list:
-                TestLoadCheckpoint._save_checkpoint(name)
+                TestLoadCheckpoint._save_checkpoint(name, checkpoint_dir=checkpoint_dir)
         else:
-            TestLoadCheckpoint._save_checkpoint(args.save_checkpoint)
+            TestLoadCheckpoint._save_checkpoint(
+                args.save_checkpoint,
+                checkpoint_dir=checkpoint_dir,
+            )
 
 
 if __name__ == "__main__":
